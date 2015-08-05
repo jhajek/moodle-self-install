0da1afdb0ce8e29b87c38958b47fdbb640b85688
Jeremy Hajek <hajek@iit.edu>
2015-08-05 13:55:19 -0500

    added pull code to get the apache2 configs to overwrite the default settings

--- webserver/install.sh
+++ webserver/install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -x
+
 # Format extra space
 mkfs -t ext4 /dev/vdb
 mkdir -p /mnt/vol-01
@@ -8,8 +12,8 @@ echo "/dev/vdb    /mnt/vol-01 ext4 defaults 0 1" >> /etc/fstab
 
 #http://www.dev-metal.com/install-setup-php-5-6-ubuntu-14-04-lts/
 sudo apt-get update
-sudo apt-get install python-software-properties
-sudo apt-get install software-properties-common
+sudo apt-get -y install python-software-properties
+sudo apt-get -y install software-properties-common
 
 sudo add-apt-repository -y ppa:ondrej/php5-5.6
 sudo add-apt-repository -y ppa:adiscon/v8-stable
@@ -17,9 +21,10 @@ sudo add-apt-repository -y ppa:adiscon/v8-stable
 sudo apt-get update
 
 # Client
-# git client needs some other dependencies
-sudo apt-get install  git curl wget rsync vim
+# git client and some other tools
+sudo apt-get install git curl wget rsync vim
 
+# Applications needed for installing moodle with dependencies
 sudo apt-get install apache2 php5 graphviz aspell php5-pspell php5-curl php5-gd php5-intl php5-mysql php5-xmlrpc php5-ldap mariadb-client
 
 # pull source from Git
@@ -40,20 +45,28 @@ sudo apt-get install ganglia-monitor
 
 #modify location
 mkdir -p /mnt/vol-01/moodle
+mkdir -p /mnt/vol-01/moodledata
+
 cp -R ~/moodle /mnt/vol-01/moodle
 mkdir -p /mnt.vol-01/moodledata
+
 chown -R www-data /mnt/vol-01/moodledata
 chown -R www-data /mnt/vol-01/moodle
+
 chmod -R 777 /mnt/vol-01/moodledata
 chmod -R 0755 /mnt/vol-01/moodle
 
-# Adjust the apache conf.d to serve out of /mnt/vol-01/moodle or else nothing happens...:
-# to be done
+# Pull down the new apache conf.d to serve out of /mnt/vol-01/moodle or else nothing happens...:
+cp ~/apache2.conf /etc/apache2/apache2.conf
+cp ~/000-default.conf /etc/apache2/sites-available/000-default.conf
+# restart service to re-read the changes
+service apache2 restart
+
 
 # now automate the site install 
- VAR=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+PUBLICURL=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
  HTTP="http://"
-# IP of remote database server
- REMOTEURL="192.168.119.75"
+# IP of remote database server - you need to retrieve this from the database install
+REMOTEURL="192.168.119.75"
 
- sudo -u www-data /usr/bin/php admin/cli/install.php --chmod=2770 --lang=en --wwwroot=$HTTP$VAR --dataroot=/mnt/vol-01/moodledata --dbtype=mariadb --dbhost=$REMOTEURL --dbuser=moodleuser --dbpass=Letmein --fullname="Greatest Site Ever" --shortname="Da Site" --adminuser=adminjrh --adminpass=Letmein1! --non-interactive --agree-license
+ sudo -u www-data /usr/bin/php admin/cli/install.php --chmod=2770 --lang=en --wwwroot=$HTTP$PUBLICURL --dataroot=/mnt/vol-01/moodledata --dbtype=mariadb --dbhost=$REMOTEURL --dbuser=moodleuser --dbpass=Letmein --fullname="Greatest Site Ever" --shortname="Da Site" --adminuser=adminjrh --adminpass=Letmein1! --non-interactive --agree-license
