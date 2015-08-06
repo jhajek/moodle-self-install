489a92ed33c4b2ea23c48d447fe478b821bd6593
Jeremy Hajek <hajek@iit.edu>
2015-08-06 10:32:20 -0500

    corrected paths

--- webserver/config-moodle.sh
+++ webserver/config-moodle.sh
@@ -10,13 +10,13 @@ set -x
  sudo apt-get update
  sudo apt-get install moosh
  
-moosh plugin-fetchinfo > ~/plugins.json
+moosh plugin-fetchinfo -p /mnt/vol-01/moodle > ~/plugins.json
 # (run it first to get plugin shortnames -- takes a while) 
  
 # make sure this directory exists to save 3 days of head scratching why the plugin-install fails
  mkdir -p /root/.moosh/moodleplugins
 
- moosh plugin-list theme_essential
- moosh plugin-install theme_essential 19
+ moosh -n plugin-list | grep theme_essential
+ moosh -n plugin-install theme_essential 19
 
 # All done
